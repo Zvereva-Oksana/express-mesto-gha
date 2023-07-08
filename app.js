fafe6452faeb26c05a869aea3f61e4f91f65a28e
Oksana Zvereva <ZverevaOksanaS@yandex.ru>
2023-07-08 16:42:11 +0300

    app: handling the wrong path

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
+const availableRoutes = ['/users', '/cards'];
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,7 +14,11 @@ app.use((req, res, next) => {
   req.user = {
     _id: '64a57faf937bd2dd83dba314',
   };
-  next();
+  if (availableRoutes.includes(req.path)) {
+    next();
+  } else {
+    res.status(404).send({ message: 'Несуществующий роут' });
+  }
 });
 app.use('/users', userRouter);
 app.use('/cards', cardRouter);
