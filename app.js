d89d535edf26d0875b821fd93bac8c928fb6c0f4
Oksana Zvereva <ZverevaOksanaS@yandex.ru>
2023-07-08 16:57:17 +0300

    fix error

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
-const availableRoutes = ['/users', '/cards'];
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,14 +13,14 @@ app.use((req, res, next) => {
   req.user = {
     _id: '64a57faf937bd2dd83dba314',
   };
-  if (availableRoutes.includes(req.path)) {
-    next();
-  } else {
-    res.status(404).send({ message: 'Несуществующий роут' });
-  }
+  next();
 });
+
 app.use('/users', userRouter);
 app.use('/cards', cardRouter);
+app.use('*', (req, res) => {
+  res.status(404).send({ message: 'Несуществующий роут' });
+});
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
--- helpers/utils.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line consistent-return
-module.exports.isIdValidity = (id, res) => {
-  if (!id.match(/^[0-9a-fA-F]{24}$/)) {
-    return res.status(404).send({ message: 'Передан несуществующий _id.' });
-  }
-};
